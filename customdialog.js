d694f4710a8542e9c7e08e7ce4fd8c13b271cf71
yutongwang0418@outlook.com <yutongwang0418@outlook.com>
2023-03-02 14:45:04 -0800

    1

--- customdialog.js
+++ customdialog.js
@@ -1,33 +1,18 @@
-function clickBtm() {
-    let b1 = document.getElementById("btn1");
-    b1.addEventListener("click", () => alert("You are starting to enter your name"));
+const showButton = document.getElementById('showDialog');
+const favDialog = document.getElementById('favDialog');
+const outputBox = document.querySelector('output');
+const selectEl = favDialog.querySelector('select');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
 
-    let b2 = document.getElementById("btn2");
-    b2.addEventListener("click", myConfirm);
-
-    let b3 = document.getElementById("btn3");
-    b3.addEventListener("click", myPrompt);
-}
-
-function myConfirm() {
-    if (confirm("Are you sure?") ) {
-        let o1 = document.getElementById("out1");
-        o1.innerHTML = "confirm result: true"
-    }
-    else {
-        let o1 = document.getElementById("out1");
-        o1.innerHTML = "confirm result: false"
-    }
-}
-
-function myPrompt() {
-    let p1 = prompt("Please enter your name", "John Wang");
-    if (p1 != null) {
-        document.getElementById("out1").innerHTML = "Your name is: " + p1;
-    }
-    else {
-        document.getElementById("out1").innerHTML = "User did not enter anything";
-    }
-}
-
-clickBtm();
+// "Show the dialog" button opens the <dialog> modally
+showButton.addEventListener('click', () => {
+    favDialog.showModal();
+});
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener('change', (e) => {
+  confirmBtn.value = selectEl.value;
+});
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+favDialog.addEventListener('close', () => {
+  outputBox.value = `ReturnValue: ${favDialog.returnValue}.`;
+});
