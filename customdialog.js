d9d198fa6c18f9c8640a966290e0e4991d85deeb
yutongwang0418@outlook.com <yutongwang0418@outlook.com>
2023-03-02 22:30:14 -0800

    p2

--- customdialog.js
+++ customdialog.js
@@ -1,4 +1,20 @@
 export function clickYes(){
-    let o1 = document.getElementById("out1");
-    o1.value = "confirm result: true"
+    document.getElementById("out1").innerHTML = "confirm result: true";
+}
+
+export function clickNo(){
+    document.getElementById("out1").innerHTML = "confirm result: false"
+}
+
+export function clickFinish(){
+    let inputBox1 = document.querySelector("#inp1");
+    let item1 = inputBox1.value;
+    // document.getElementById("out1").innerHTML = `Your name is: ${item1}`;
+    if(item1 === "") {
+        document.getElementById("out1").innerHTML = "User did not enter anything";
+    }
+    else {
+        let clean1 = DOMPurify.sanitize(item1);
+        document.getElementById("out1").innerHTML = `Your name is: ${clean1}`;
+    }
 }
